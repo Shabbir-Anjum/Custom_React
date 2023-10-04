28d8f849ec5178da3f5a29d47365d36d7b957cb1
Shabbir-Anjum <mahrshabbir768@gmail.com>
2023-10-04 20:58:20 +0500

    Child Access

--- CustomReact/customRender.js
+++ CustomReact/customRender.js
@@ -1,9 +1,12 @@
 function customRender(reactElement, container){
     const domElement= document.createElement(reactElement.type)
     document.innerHTML= reactElement.children
-    domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
-    container.appendChild(dom)
+   for(const prop in reactElement.props){
+    if(prop=='children') continue;
+    domElement.setAttribute(prop, reactElement.props[prop])
+   }
+   
+    container.appendChild(domElement)
 }
 const reactElement= {
     type: 'a',
